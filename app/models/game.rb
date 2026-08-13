905f2b25f7b04cdfb807ab3e7a0fc02724c13f2e
Connor Hughes <connor.hughes@platform45.com>
2026-08-13 09:52:45 +0200

    Linting

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,9 +4,9 @@ class Game < ApplicationRecord
   broadcasts_to ->(game) { game }, partial: 'games/board'
 
   WIN_COMBINATIONS = [
-    [0, 1, 2], [3, 4, 5], [6, 7, 8], # rows
-    [0, 3, 6], [1, 4, 7], [2, 5, 8], # columns
-    [0, 4, 8], [2, 4, 6],             # diagonals
+    [ 0, 1, 2 ], [ 3, 4, 5 ], [ 6, 7, 8 ], # rows
+    [ 0, 3, 6 ], [ 1, 4, 7 ], [ 2, 5, 8 ], # columns
+    [ 0, 4, 8 ], [ 2, 4, 6 ]             # diagonals
   ].freeze
 
   def cells
