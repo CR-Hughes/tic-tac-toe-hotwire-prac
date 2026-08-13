f1c712e39b9623991bbb8d06299778ea1198a789
Connor Hughes <connor.hughes@platform45.com>
2026-08-13 09:38:26 +0200

    Broadcasts to play between browsers

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,8 @@
 class Game < ApplicationRecord
   has_many :moves, dependent: :destroy
 
+  broadcasts_to ->(game) { game }, partial: "games/board"
+
   WIN_COMBINATIONS = [
     [ 0, 1, 2 ], [ 3, 4, 5 ], [ 6, 7, 8 ], # rows
     [ 0, 3, 6 ], [ 1, 4, 7 ], [ 2, 5, 8 ], # columns
--- app/models/move.rb
+++ app/models/move.rb
@@ -1,3 +1,5 @@
 class Move < ApplicationRecord
   belongs_to :game
+
+  broadcasts_to :game, target: "move_history"
 end
